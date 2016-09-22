118ec794d85ff56f59f88466e8d9df6965c85440
longjj <luojj26@mail2.sysu.edu.cn>
2016-09-22 21:11:26 +0800

    find a bug display a empty thing

--- week3/include/Error_code.hpp
+++ week3/include/Error_code.hpp
@@ -1,6 +1,6 @@
 #ifndef ERROR_CODE_HPP_
 #define ERROR_CODE_HPP_
 
-enum Error_code { success, fail, overflow, underflow };
+enum Error_code { fail, success, overflow, underflow };
 
 #endif
--- week3/src/main.cpp
+++ week3/src/main.cpp
@@ -3,12 +3,19 @@
 
 void help() {
   std::cout << "----------------Welcome!!----------------" << std::endl;
-  std::cout << "0 push an element into the stack" << std::endl;
-  std::cout << "1 pop an element from the stack" << std::endl;
-  std::cout << "2 display the current stack" << std::endl;
-  std::cout << "3 push an element into the queue" << std::endl;
-  std::cout << "4 pop an element from the queue" << std::endl;
-  std::cout << "5 display the current stack" << std::endl;
+  std::cout << "0 display the help menu" << std::endl;
+  std::cout << "1 push an element into the stack" << std::endl;
+  std::cout << "2 pop an element from the stack" << std::endl;
+  std::cout << "3 display the current stack" << std::endl;
+  std::cout << "4 push an element into the queue" << std::endl;
+  std::cout << "5 pop an element from the queue" << std::endl;
+  std::cout << "6 display the current stack" << std::endl;
+  std::cout << "------------------------------------------" << std::endl;
+  std::cout << "Here is the Error_code:" << std::endl;
+  std::cout << "0 for fail" << std::endl;
+  std::cout << "1 for success" << std::endl;
+  std::cout << "2 for overflow" << std::endl;
+  std::cout << "3 for underflow" << std::endl;
 }
 
 int main(int argc, char const *argv[]) {
@@ -33,23 +40,32 @@ int main(int argc, char const *argv[]) {
     while (std::cin >> choice) {
       switch (choice) {
         case 0:
+          help();
+          break;
+        case 1:
+          std::cout << "Input a data:" << std::endl;
           std::cin >> data;
           std::cout << s.push(data) << std::endl;
           break;
-        case 1:
+        case 2:
+          std::cout << "Pop a data:" << std::endl;
           std::cout << s.pop() << std::endl;
           break;
-        case 2:
+        case 3:
+          std::cout << "Display my stack:" << std::endl;
           std::cout << s.display() << std::endl;
           break;
-        case 3:
+        case 4:
+          std::cout << "Input a data:" << std::endl;
           std::cin >> data;
           std::cout << q.push(data) << std::endl;
           break;
-        case 4:
+        case 5:
+          std::cout << "Pop a data:" << std::endl;
           std::cout << q.pop() << std::endl;
           break;
-        case 5:
+        case 6:
+          std::cout << "Display my queue:" << std::endl;
           std::cout << q.display() << std::endl;
           break;
         default:
