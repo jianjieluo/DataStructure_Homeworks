1703a136f0ce813fcbedc9d3bfc29ce87b7e4da9
longjj <luojj26@mail2.sysu.edu.cn>
2016-10-20 08:50:10 +0800

    finish the first version of 1004

---
+++ week7/sicily/1004.cpp
@@ -0,0 +1,55 @@
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool isRight(const string& lhs, const string& rhs) {
+  if (lhs.length() != rhs.length()) return false;
+  const char* a = lhs.c_str();
+  const char* b = rhs.c_str();
+  for (unsigned int i = 0; i < lhs.length(); ++i) {
+    int temp = a[i] + b[i];
+    // cout << a[i] << b[i];
+    if (temp != 149 && temp != 138) {
+      return false;
+    }
+  }
+  return true;
+}
+
+int main(int argc, char const* argv[]) {
+  int n;
+  cin >> n;
+  std::vector<string> v;
+  while (n--) {
+    v.clear();
+    int m;
+    cin >> m;
+    while (m--) {
+      string temp;
+      cin >> temp;
+      v.push_back(temp);
+    }
+
+    m = v.size();
+    bool* color = new bool[m];
+    memset(color, 0, m);
+    int count = 0;
+    for (int i = 0; i < m - 1; ++i) {
+      if (color[i]) continue;
+      for (int j = i + 1; j < m; ++j) {
+        if (color[j]) continue;
+        if (isRight(v[i], v[j])) {
+          ++count;
+          color[i] = true;
+          color[j] = true;
+        }
+      }
+    }
+    cout << count << endl;
+    delete[] color;
+  }
+  return 0;
+}
