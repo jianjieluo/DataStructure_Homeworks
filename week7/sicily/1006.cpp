71b48b8334b075a0d085b9b1f130da173de96034
longjj <luojj26@mail2.sysu.edu.cn>
2016-10-20 19:46:55 +0800

    finish the 1006 first version

---
+++ week7/sicily/1006.cpp
@@ -0,0 +1,47 @@
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <vector>
+
+inline bool isNum(const char ch) { return (ch >= '0' && ch <= '9'); }
+
+int getLen(const std::vector<int>& v) {
+  int n = v.size();
+  int times = 0;
+  for (int i = 0; i < n; ++i) {
+    times += v[i] * pow(10, n - 1 - i);
+  }
+  return times;
+}
+
+int main(int argc, char const* argv[]) {
+  std::string str;
+  while (1) {
+    std::cin >> str;
+    if (str == "XXX") break;
+
+    for (unsigned int i = 0; i < str.size(); ++i) {
+      if (!isNum(str[i])) {
+        std::cout << str[i];
+      } else {
+        std::vector<int> len;
+        for (unsigned int j = i; j < str.size(); ++j) {
+          if (isNum(str[j])) {
+            len.push_back(str[j] - '0');
+          } else {
+            break;
+          }
+        }
+
+        int times = getLen(len);
+        for (int j = 1; j < times; ++j) {
+          std::cout << str[i - 1];
+        }
+        i = i + len.size() - 1;
+      }
+    }
+    std::cout << std::endl;
+  }
+  return 0;
+}
